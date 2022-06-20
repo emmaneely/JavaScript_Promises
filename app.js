7fdaf740b82889e6ce880768fbf4bf1d67879f3a
emmaneely <emmaneely3@gmail.com>
2022-06-20 15:01:24 -0500

    Completed exercise 1

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let watching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream;
+    if (userWatchingLiveStream == true) {
+        resolve("Thumbs up and Subscribe!");
+    } else {
+        reject({
+            name: "User Left",
+            message: ":(",
+        })
+    }
+});
+
+watching
+.then(message => console.log(message))
+.catch(error => console.log(error.name + " " + error.message));
